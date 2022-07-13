856243b2e514cc84f9f855eee709fb473cb57f98
KonainB <107124436+KonainB@users.noreply.github.com>
2022-07-13 09:48:51 +0100

    Create home.java

---
+++ home.java
@@ -0,0 +1,16 @@
+package Example;
+
+public class Calculator {
+	static double radius;
+	final double pi=3.14;
+	
+	public double areaOfCircle()
+	{
+		return (pi*radius*radius);
+	}
+	
+	
+	
+			
+
+}
